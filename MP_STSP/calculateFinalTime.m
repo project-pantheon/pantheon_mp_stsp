880d8ab822ce261b16ba268029a4bb5a6e34ce2b
renzo.carpio <renzo.carpio@hotmail.it>
2020-10-20 00:26:39 +0200

    removed depot time

--- MP_STSP/calculateFinalTime.m
+++ MP_STSP/calculateFinalTime.m
@@ -2,7 +2,7 @@ function T = calculateFinalTime(sol_edges,sel_vertex,Vr,c_req)
 
     %sel_vertex = sel_vertex(2:end); %take out depot
 
-    t1 = 5*sum(sol_edges); %griglia sempre a 5x5
+    t1 = 5*sum(sol_edges-2); %griglia sempre a 5x5
     
     %find stops
     [~,pos] = intersect(Vr,sel_vertex) ;
